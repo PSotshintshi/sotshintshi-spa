34d75185c2fe204a40e222589d10abd036b3ea08
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-08-15 23:07:52 +0200

    set up Vite React project with Tailwind and Spa pages

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-center">
+        Your Escape to Peace & Wellness
+      </h1>
+    </div>
+  );
+};
+
+export default App;
